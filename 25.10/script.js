fe81020548bffcf19949ad8024cc2abe532cf000
Kutman <kutmansagymbekuulu@gmail.com>
2022-10-25 11:03:21 +0600

    new file 25.10

---
+++ 25.10/script.js
@@ -0,0 +1,33 @@
+/***Task-1***/
+
+let a = 17;
+let b = 10;
+let c = a-b;
+console.log(c);
+
+let d = 7;
+let result = c+d;
+console.log(result);
+
+/****Task-2****/
+
+let hour = 10;
+let minute = 40;
+let second = 27;
+let time = `${hour}:${minute}:${second}`;
+console.log(time);
+
+/****Task-3****/
+
+let num = 10;
+let result2 = num++;
+let result3 = ++num;
+alert(num);
+
+
+
+
+
+
+
+
