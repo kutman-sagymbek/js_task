ced96267847d47aae8cf189140f6d34f20cc6482
Kutman <kutmansagymbekuulu@gmail.com>
2022-10-27 12:16:49 +0600

    new file 27-10

---
+++ 27-10/script.js
@@ -0,0 +1,32 @@
+/**** Task-1 *****/
+
+let capital_of_KR = prompt('Capital of KR?', 'Capital');
+if(capital_of_KR === 'Bishkek'){
+    alert('You are right');
+} else{
+    alert('Do not you know the capital of KR?');
+}
+
+console.log(capital_of_KR);
+
+
+/******Task-2*****/
+
+let someone = prompt('Who is there', 'Name');
+if (someone === 'admin'){
+    let password = prompt('Password', 1234);
+    if (password === '5555'){
+        alert('Welcome');
+    } else if (password === null){
+        alert('Cancelled');
+    } else {
+        alert('Incorrect password');
+    }
+} else if(someone === null){
+    alert('Cancelled');
+} else {
+    alert('I do not know you');
+}
+
+
+
