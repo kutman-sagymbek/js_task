f31ea4ceffc5d42f4341a28c2f2fdf1a2e6c8db7
Kutman <kutmansagymbekuulu@gmail.com>
2022-10-31 11:21:51 +0600

    31-10

---
+++ 31-10/script.js
@@ -0,0 +1,58 @@
+    /****** Task-1 *******/
+
+console.log(userSalaries[0].salary);
+console.log(userSalaries[1].salary);
+
+let userSalaries = {
+    Ella: 2000,
+    Sophie: 2700,
+    Ellie: 2300
+}
+
+console.log(userSalaries.Ella);
+console.log(userSalaries.Sophie);
+
+    /******** Task-2 ********/
+
+let objects = {
+    a: 5,
+    b: 6,
+    c: 10
+}
+
+let sum = 0;
+for (let key in objects) {
+    sum += objects[key];
+}
+alert(sum);
+
+    /******** Task-3 *********/
+
+let data = false;
+let number = data ? 3 : 5;
+console.log(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
