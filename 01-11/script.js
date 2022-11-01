cbbd9c983814067b8fe22267d2ce93cc4b702ee8
Kutman <kutmansagymbekuulu@gmail.com>
2022-11-01 12:01:09 +0600

    01-10

--- 01-11/script.js
+++ 01-11/script.js
@@ -24,10 +24,10 @@ let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 let newNumbers = []
 for( let i = 0; i < numbers.length; i++){
     if ((numbers[i]%2) === 0) {
-        console.log(numbers[i]);
+        newNumbers.push(numbers[i]);
     }
-    newNumbers.push(i);
 }
+console.log(newNumbers);
 
 
 
