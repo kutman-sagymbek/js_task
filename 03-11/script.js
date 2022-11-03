d49c3aa7d54dfd521e3750b984b1088abdd2cc7a
Kutman <kutmansagymbekuulu@gmail.com>
2022-11-03 23:47:02 +0600

    03-11

--- 03-11/script.js
+++ 03-11/script.js
@@ -1,24 +1,24 @@
 /******* Task-1 ********/
 
-const numbers = [1,2,3];
-const doubleNumbers = numbers.map(item => item * 2);
-console.log(doubleNumbers);
+// const numbers = [1,2,3];
+// const doubleNumbers = numbers.map(item => item * 2);
+// console.log(doubleNumbers);
 
 /******* Task-2 ********/
 
-function fill_array(value, size) {
+function fill_array(value = 'a', size = 3) {
     let new_array = [];
     for (let i = 0; i < size; i++) {
         new_array.push(value);
     }
     return new_array;
 }
-let result = fill_array('a', 3);
+let result = fill_array();
 console.log(result);
 
 
 /******* Task-3 ********/
 
-const justNumbers = [1,2,3,4,5];
-const reverseNumbers = justNumbers.reverse();
-console.log(reverseNumbers);
+// const justNumbers = [1,2,3,4,5];
+// const reverseNumbers = justNumbers.reverse();
+// console.log(reverseNumbers);
