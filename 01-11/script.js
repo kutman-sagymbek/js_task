7a7d031299b94b60f895d2d6ae05c9163e1f4e59
Kutman <kutmansagymbekuulu@gmail.com>
2022-11-03 09:01:35 +0600

    02-11

--- 01-11/script.js
+++ 01-11/script.js
@@ -1,34 +1,47 @@
-let numbers = [1, 2, 5, 6, 88, 5];
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-}
-
-console.log(sum);
+// let numbers = [1, 2, 5, 6, 88, 5];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+// }
+//
+// console.log(sum);
 
 
 
 /******* Task-2 *******/
-
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i] * numbers[i];
-}
-
-console.log(sum);
+//
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i] * numbers[i];
+// }
+//
+// console.log(sum);
 
 /******* Task-3 *******/
 
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
-let newNumbers = []
-for( let i = 0; i < numbers.length; i++){
-    if ((numbers[i]%2) === 0) {
-        newNumbers.push(numbers[i]);
+// let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// let newNumbers = []
+// for( let i = 0; i < numbers.length; i++){
+//     if ((numbers[i]%2) === 0) {
+//         newNumbers.push(numbers[i]);
+//     }
+// }
+// console.log(newNumbers);
+
+let anyNumber = [42, 65, 49, 68, 56, 47, 42, 51, 35, 58, 63, 40, 70];
+
+let maximum = 70;
+
+let empty = [];
+
+for(i = 0; i < anyNumber.length; i++) {
+    if(anyNumber[i] === maximum){
+        empty.push(anyNumber[i]);
     }
 }
-console.log(newNumbers);
 
+console.log(empty);
 
 
 
---
+++ 02-11/script.js
@@ -0,0 +1,44 @@
+/******* Task-1 ********/
+
+function minNumber(a, b) {
+    if (a < b){
+        return a;
+    } else {
+        return b;
+    }
+}
+
+let result = minNumber(90, 75);
+
+function secondNumber(result){
+    console.log(result);
+}
+secondNumber(result);
+
+
+/******* Task-2 ********/
+
+function hello(a) {
+    alert(a);
+}
+hello('hello');
+
+function number(a){
+    return a * a * a;
+}
+
+let result = number(30);
+alert(result);
+
+
+/******* Task-1 ********/
+
+function getName(name, myGuest){
+    if(name !== undefined) {
+        return name;
+    } else {
+        return myGuest;
+    }
+}
+let name = getName('Hi, Asan', 'Hi, guest');
+alert(name);
